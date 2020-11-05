583215ac79be6685fdc0d8c69753c1a7e7801123
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-05 13:01:13 -0500

    update

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import json
 import time
 from datetime import datetime
 
@@ -18,6 +17,7 @@ def convert_datatype(entry):
             return date
     return entry['value']
 
+
 def sleepy(r):
     print('status code:', r.status_code, r.reason)
     print('Sleeping... (1 minute)')
@@ -30,21 +30,22 @@ def wikidata_query(query):
     r = requests.get(url, params={'format': 'json', 'query': query})
     if r.status_code != 200:
         sleepy(r)
+        return pd.DataFrame()  # return empty dataframe
     else:
         try:
             data = r.json()
         except Exception as ex:
             print(ex)
             sleepy(r)
-        # except json.JSONDecodeError as e:
-        #     raise Exception('Invalid query')
+    # except json.JSONDecodeError as e:
+    #     raise Exception('Invalid query')
 
         if ('results' in data) and ('bindings' in data['results']):
             columns = data['head']['vars']
             rows = []
             for binding in data['results']['bindings']:
                 row = [convert_datatype(binding[col]) if col in binding else None
-                   for col in columns]
+                       for col in columns]
                 rows.append(row)
         else:
             raise Exception('No results')
