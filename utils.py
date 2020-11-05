6d1457be8eadac7532a66e8b05b2a1ea9ef5226d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-05 12:56:41 -0500

    update error check

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import time
 from datetime import datetime
 
 import pandas as pd
@@ -17,26 +18,35 @@ def convert_datatype(entry):
             return date
     return entry['value']
 
+def sleepy(r):
+    print('status code:', r.status_code, r.reason)
+    print('Sleeping... (1 minute)')
+    time.sleep(60)
+
 
 def wikidata_query(query):
     url = 'https://query.wikidata.org/sparql'
-    try:
-        r = requests.get(url, params={'format': 'json', 'query': query})
-        if r.status_code == 200:
-            data = r.json()
-        else:
-            print('status code:', r.status_code, r.reason)
-    except json.JSONDecodeError as e:
-        raise Exception('Invalid query')
-
-    if ('results' in data) and ('bindings' in data['results']):
-        columns = data['head']['vars']
-        rows = []
-        for binding in data['results']['bindings']:
-            row = [convert_datatype(binding[col]) if col in binding else None
-                   for col in columns]
-            rows.append(row)
+    # try:
+    r = requests.get(url, params={'format': 'json', 'query': query})
+    if r.status_code != 200:
+        sleepy(r)
     else:
-        raise Exception('No results')
+        try:
+            data = r.json()
+        except Exception as ex:
+            print(ex)
+            sleepy(r)
+        # except json.JSONDecodeError as e:
+        #     raise Exception('Invalid query')
 
-    return pd.DataFrame(rows, columns=columns)
+        if ('results' in data) and ('bindings' in data['results']):
+            columns = data['head']['vars']
+            rows = []
+            for binding in data['results']['bindings']:
+                row = [convert_datatype(binding[col]) if col in binding else None
+                   for col in columns]
+                rows.append(row)
+        else:
+            raise Exception('No results')
+
+        return pd.DataFrame(rows, columns=columns)
